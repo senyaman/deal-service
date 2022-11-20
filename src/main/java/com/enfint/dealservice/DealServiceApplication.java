f0d94643c47582f8c476e2e9500bed63df5e9e1b
Senya- Man <masenyagift0@gmail.com>
2022-11-20 17:04:31 +0200

    added modelMapper to convert between Entities and DTOs

--- src/main/java/com/enfint/dealservice/DealServiceApplication.java
+++ src/main/java/com/enfint/dealservice/DealServiceApplication.java
@@ -1,11 +1,17 @@
 package com.enfint.dealservice;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class DealServiceApplication {
 
+	@Bean
+	public ModelMapper modelMapper() {
+		return new ModelMapper();
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(DealServiceApplication.class, args);
 	}
